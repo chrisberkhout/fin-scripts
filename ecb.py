65fec2e8a951ebdcda278f9e9480a7e814334d21
Chris Berkhout <chrisberkhout@gmail.com>
2015-06-28 20:01:07 +0200

    Initial commit of ECB script.

---
+++ ecb.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+
+# ECB Euro foreign exchange reference rates
+# https://www.ecb.europa.eu/stats/exchange/eurofxref/html/index.en.html
+
+import piecash
+from decimal import Decimal
+from datetime import *
+from urllib import request
+from xml.etree import ElementTree
+
+# config
+
+book_file   = '/home/chris/Documents/Finances/SCRATCH.gnucash'
+source      = 'ECB Euro foreign exchange reference rates'
+# source_url  = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml' # last 90 days
+source_url  = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml' # since 1999
+start_date  = date(datetime.utcnow().date().year - 1, 1, 1)
+commodities = { 'AUD' }
+
+
+# open book
+
+book = piecash.open_book(book_file, readonly=False)
+
+
+# determine uncovered dates per commodity
+
+def prices_for(book, commodity_mnemonic, source):
+    currency = book.currencies(mnemonic='EUR')
+    commodity = book.currencies(mnemonic=commodity_mnemonic)
+    return {
+        x for x in book.prices if
+            x.currency == currency
+            and x.commodity == commodity
+            and x.source == source
+    }
+
+dates_covered = dict()
+for commodity in commodities:
+    dates_covered[commodity] = {
+        x.date.astimezone(timezone.utc).date() for x in
+            prices_for(book, commodity, source)
+    }
+
+date_today = datetime.utcnow().date()
+days_since_start = (date_today - start_date).days
+
+dates_to_cover = {
+    start_date + timedelta(x) for x in range(days_since_start + 1)
+}
+
+dates_uncovered = dict()
+for commodity in commodities:
+    dates_uncovered[commodity] = dates_to_cover - dates_covered[commodity]
+
+
+# get data
+
+url = request.urlopen(source_url)
+root = ElementTree.fromstring(url.read())
+url.close()
+
+
+# read data, add to book if missing
+
+ns = {
+    'default': 'http://www.ecb.int/vocabulary/2002-08-01/eurofxref',
+    'gesmes': 'http://www.gesmes.org/xml/2002-08-01'
+}
+
+for commodity_mnemonic in commodities:
+    currency = book.currencies(mnemonic='EUR')
+    commodity = book.currencies(mnemonic=commodity_mnemonic)
+    rate_xpath = "./*[@currency='%s']" % (commodity_mnemonic)
+    for day in root.find('default:Cube', ns):
+        d = date(*[ int(x) for x in day.attrib['time'].split('-') ])
+        rate = (1 / Decimal(day.find(rate_xpath).attrib['rate'])).quantize(Decimal('1.0000000'))
+        if (d in dates_uncovered[commodity_mnemonic]):
+            dt = datetime(d.year, d.month, d.day, 13, 0, 0, 0, timezone.utc)
+            price = piecash.core.commodity.Price(commodity, currency, dt, rate, 'unknown', source)
+            book.add(price)
+            print("added %s" % (str(price)))
+
+
+# save and finish
+
+book.save()
+
+
+#################################################################################
+#################################################################################
+#
+#book = piecash.open_book(book_file, readonly=False)
+#
+#for price in book.prices:
+#  print(price)
+#
+#eur = book.currencies(mnemonic='EUR')
+#aud = book.currencies(mnemonic='AUD')
+#xbt = book.currencies(mnemonic='XBT')
+#
+## naive datetimes are treated as local, converted back to UTC for storage, and converted again to local on read
+#p = piecash.core.commodity.Price(xbt, aud, datetime.now(), decimal.Decimal('2.777777'), 'last', 'from my script')
+#
+## aware datetime in UTC will be written without conversion (but still converted into local on read)
+#dt = datetime(2015,6,1,0,0,0,0,timezone.utc)
+#p = piecash.core.commodity.Price(xbt, aud, dt, decimal.Decimal('2.444444'), 'last', 'from my script')
+#
+#book.add(p)
+#book.save()
+#
+## to get a date from local into UTC (as it was/will be stored), do
+#book.prices[-1].date.astimezone(timezone.utc)
+#
+#################################################################################
+#
+#from urllib import request
+#import xml.etree.ElementTree as ET
+#
+## ECB Euro foreign exchange reference rates
+## https://www.ecb.europa.eu/stats/exchange/eurofxref/html/index.en.html
+#
+#url = request.urlopen('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml') # last 90 days
+#
+#url = request.urlopen('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml') # since 1999
+#
+#root = ET.fromstring(url.read())
+#url.close()
+#
+#ns = {
+#    'default': 'http://www.ecb.int/vocabulary/2002-08-01/eurofxref',
+#    'gesmes': 'http://www.gesmes.org/xml/2002-08-01'
+#}
+#
+#for day in root.find('default:Cube', ns):
+#    print(day.attrib['time'], day.find("./*[@currency='AUD']").attrib['rate'])
+#
+#
+#################################################################################
+#
+## start_dt = datetime.datetime(2014,1,1,0,0,0,0,datetime.timezone.utc)
+## today_dt = datetime.datetime.now(datetime.timezone.utc)
+## days_since_start = (today_dt - start_dt).days
+#
+#start_d = datetime.date(2014,1,1)
+#today_d = datetime.datetime.utcnow().date()
+#days_since_start = (today_d - start_d).days
+#
+#{ start_d + datetime.timedelta(x) for x in range(days_since_start + 1) }
+#
+#[ start_d + datetime.timedelta(x) for x in range(days_since_start + 1) ]
+#
+#{ str(start_d + datetime.timedelta(x)) for x in range(days_since_start + 1) }
+#
+#[ str(start_d + datetime.timedelta(x)) for x in range(days_since_start + 1) ]
+#
+#4 not in {1,2,3,45}
+#
+#4 in {1,2,3,4,5}
+#
+#d = dict()
+#d[xbt] = 'test'
+#d[33] = 'test'
+#d[today] = 'hello'
+#d
+## {33: 'test', datetime.datetime(2015, 6, 27, 19, 44, 23, 632517, tzinfo=datetime.timezone.utc): 'hello'}
+#
